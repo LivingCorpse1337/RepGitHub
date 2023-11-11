83a67b9da8cbe2b3b006e539f39dafb717ee6e29
LivingCorpse1337 <138614814+LivingCorpse1337@users.noreply.github.com>
2023-11-11 20:00:18 +0300

    Update custom_filters.py

--- NewsPaper1837/news/templatetags/custom_filters.py
+++ NewsPaper1837/news/templatetags/custom_filters.py
@@ -9,9 +9,8 @@ cens = ['чиновники', 'дуэль', 'император']
 
 def censor(word):
    if isinstance(word, str):
-      for i in word.split():
-         if i in cens:
-            word = word.replace(i[1:], '*' * len(i[1:]))
+      for i in cens:
+         word = word.replace(i[1:], '*' * len(i[1:]))
    else:
       raise ValueError(
          'custom_filters -> censor -> A string is expected, but a different data type has been entered')
