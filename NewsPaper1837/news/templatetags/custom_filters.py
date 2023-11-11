1f108c742dd02c9307b6e068d17d77c09ddead2e
LivingCorpse1337 <138614814+LivingCorpse1337@users.noreply.github.com>
2023-11-11 16:36:11 +0300

    Add files via upload

---
+++ NewsPaper1837/news/templatetags/custom_filters.py
@@ -0,0 +1,19 @@
+from django import template
+
+
+register = template.Library()
+
+cens = ['чиновники', 'дуэль', 'император']
+
+@register.filter()
+
+def censor(word):
+   if isinstance(word, str):
+      for i in word.split():
+         if i in cens:
+            word = word.replace(i[1:], '*' * len(i[1:]))
+   else:
+      raise ValueError(
+         'custom_filters -> censor -> A string is expected, but a different data type has been entered')
+   return word
+
