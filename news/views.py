13ff28ed6dd1c13a74e6a65817258aa72e572219
LivingCorpse1337 <138614814+LivingCorpse1337@users.noreply.github.com>
2024-01-25 14:21:44 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,94 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category
+from datetime import datetime
+from .filters import PostFilter
+from .forms import PostForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import TemplateView
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'head'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
+class PostCreate(PermissionRequiredMixin,  CreateView):
+    permission_required = ('news.add_post')
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.path == '/articles/create/':
+            post.post = 'AR'
+        post.save()
+        return super().form_valid(form)
+
+class PostUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_post')
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_post')
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class ArticleUpdate(LoginRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+
+class ArticleDelete(DeleteView):
+    model = Post
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class CategoryList(PostsList):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id = self.kwargs['pk'])
+        queryset = Post.objects.filter(post_category = self.category).order_by('timing_post')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+@login_required
+def subscribe(request, pk):
+    user = request.user
+    category = Category.objects.get(id = pk)
+    category.subscribers.add(user)
+
+    message = 'Вы подписались на рассылку'
+    return render(request, 'news/subscribe.html', {'category': category, 'message': message})
